81f30bc46bc9f816e230898c65eb07a99263dc98
Pipe <felipe.sniper14@gmail.com>
2022-12-18 20:09:47 -0300

    nuevo test

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Proyectos UTEM\taller3_distribuidos"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Pipe\Desktop\taller3-distribuidos"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- lib/Ventanas/lista_pokemones.dart
+++ lib/Ventanas/lista_pokemones.dart
@@ -5,21 +5,23 @@ import 'package:taller3_distribuidos/apis/api_pokemon.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ListaPokemones extends StatelessWidget {
-
   const ListaPokemones({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: const Color(0xffd1dee0),
-      padding: const EdgeInsets.symmetric(vertical: 20, ),
+      padding: const EdgeInsets.symmetric(
+        vertical: 20,
+      ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-
-          const SizedBox(height: 32,),
+          const SizedBox(
+            height: 32,
+          ),
           Column(
             //mainAxisSize: MainAxisSize.min,
             //mainAxisAlignment: MainAxisAlignment.start,
@@ -29,28 +31,33 @@ class ListaPokemones extends StatelessWidget {
                 "Tu PokeAPI",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                    color: Color(0xffef063f),
+                    color: Color.fromARGB(255, 0, 0, 0),
                     fontSize: 32,
                     fontFamily: "Inter",
-                    fontWeight: FontWeight.w400
-                ),
+                    fontWeight: FontWeight.w400),
               ),
-              SizedBox(height: 15,),
-
-              SizedBox(height: 15,),
-
+              SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                height: 15,
+              ),
             ],
           ),
-          const SizedBox(height: 50,),
+          const SizedBox(
+            height: 50,
+          ),
           TextButton(
-              onPressed: () {PokeApi.getPokemones(context);} ,
+              onPressed: () {
+                PokeApi.getPokemones(context);
+              },
               child: Container(
                 width: 250,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
-                    color: const Color(0xcc009850)
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    color: const Color(0xcc009850)),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                 child: Row(
                   mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -59,30 +66,26 @@ class ListaPokemones extends StatelessWidget {
                     Container(
                       width: 14,
                       height: 14,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8)
-                      ),
-
+                      decoration:
+                          BoxDecoration(borderRadius: BorderRadius.circular(8)),
                     ),
-                    const SizedBox(width: 10,),
+                    const SizedBox(
+                      width: 10,
+                    ),
                     const Text(
-                      "Ver Pokemones",
+                      "Ver Pokemons",
                       textAlign: TextAlign.center,
                       style: TextStyle(
                           color: Color(0xffedf2f4),
                           fontSize: 18,
                           fontFamily: "Inter",
-                          fontWeight: FontWeight.w700
-                      ),
+                          fontWeight: FontWeight.w700),
                     )
                   ],
                 ),
-              )
-          )
+              ))
         ],
       ),
     );
   }
 }
-
-
